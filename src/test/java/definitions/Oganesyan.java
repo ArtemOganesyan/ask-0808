e8b11f45d22b0013c759cdaaae1f59f02eeb3ba1
Artem Oganesyan <56502558+ArtemOganesyan@users.noreply.github.com>
2022-09-09 23:20:11 +0300

    New defs

---
+++ src/test/java/definitions/Oganesyan.java
@@ -0,0 +1,13 @@
+/*
+ * Created by Artem Oganesyan on 09.09.2022
+ */
+
+package definitions;
+
+import cucumber.api.java.en.Given;
+
+public class Oganesyan {
+    @Given("something")
+    public void something() {
+    }
+}
