f1f2e6cb8b921fa43e1219a7ab5827d15acd8a59
Dasha Dreizina <ddreizina.qa@gmail.com>
2022-09-11 10:56:10 -0400

    “DaryaD”

---
+++ src/test/java/definitions/DaryaD.java
@@ -0,0 +1,23 @@
+package definitions;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.io.File;
+import java.util.Date;
+import java.util.Iterator;
+import static org.assertj.core.api.Assertions.*;
+import static support.TestContext.getDriver;
+
+public class DaryaD {
+    @Given("Da Open URL {string}")
+    public void daOpenURLString(String url) {
+        getDriver().get(url);
+    }
+}
--- src/test/resources/Features/DaryaD.feature
+++ src/test/resources/Features/DaryaD.feature
@@ -3,3 +3,4 @@ Feature: Verification staff
 
   @ASK_Portnov1
   Scenario: Teacher role
+    Given Da Open URL {string}
