21f96cbd60ee3858c7b98d45ef0651df33445540
Zinaida <zkonovalova78@gmail.com>
2022-09-12 23:42:24 -0700

    update 1

--- src/test/java/definitions/konovalova.java
+++ src/test/java/definitions/konovalova.java
@@ -4,34 +4,44 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.*;
+import static java.lang.Thread.sleep;
+import static org.assertj.core.api.Assertions.assertThat;
+import static support.TestContext.getDriver;
 
 public class konovalova {
     @Given("ZK open url {string}")
-    public void zkOpenUrl(String arg0) {
-
+    public void zkOpenUrl(String url)  {
+        getDriver().get(url);
     }
 
     @When("ZK type {string} into element with xpath {string}")
-    public void zkTypeIntoElementWithXpath(String arg0, String arg1) {
+    public void zkTypeIntoElementWithXpath(String text, String xpath)
+    {getDriver().findElement(By.xpath(xpath)).sendKeys(text);
     }
 
     @Then("type ZK {string} into element with xpath {string}")
-    public void typeZKIntoElementWithXpath(String arg0, String arg1) {
+    public void typeZKIntoElementWithXpath(String text, String xpath) {
+        getDriver().findElement(By.xpath(xpath)).sendKeys(text);
     }
 
     @And("ZK click on the element with xpath {string}")
-    public void zkClickOnTheElementWithXpath(String arg0) {
+    public void zkClickOnTheElementWithXpath(String xpath) {
+        getDriver().findElement(By.xpath(xpath)).click();
     }
 
     @And("ZK wait for {int} sec")
-    public void zkWaitForSec(int arg0) {
+    public void zkWaitForSec(int sec) throws InterruptedException
+    {sleep(sec * 1000);
     }
 
     @Then("ZK element with xpath {string} is displayed")
-    public void zkElementWithXpathIsDisplayed(String arg0) {
+    public void zkElementWithXpathIsDisplayed(String xpath) {
+        assertThat(getDriver().findElement(By.xpath(xpath)).isDisplayed()).isTrue();
     }
 
     @And("click ZK on the element with xpath {string}")
-    public void clickZKOnTheElementWithXpath(String arg0) {
+    public void clickZKOnTheElementWithXpath(String xpath) {
+        getDriver().findElement(By.xpath(xpath)).click();
     }
 }
