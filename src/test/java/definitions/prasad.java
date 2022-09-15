01c7ca9327d2b4de6217365e300638e5bc60a8e1
Madhu prasad <madhup5532@gmail.com>
2022-09-15 09:51:15 -0700

    changes in first name field

--- src/test/java/definitions/prasad.java
+++ src/test/java/definitions/prasad.java
@@ -1,9 +1,77 @@
 package definitions;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import org.openqa.selenium.By;
 
 public class prasad {
     @Given("MAP open url")
     public void mapOpenUrl() {
+
+    }
+
+    @And("MAP type email and password")
+    public void mapTypeEmailAndPassword() {
+
+    }
+
+    @Given("MAP open url {string}")
+    public void mapOpenUrl(String arg0) {
+        getDriver().get("http://ask-stage.portnov.com");
+        
+    }
+
+    @Then("MAP click on Register Now button")
+    public void mapClickOnRegisterNowButton() {
+        getDriver().findElement(By.xpath("//span[contains(text(),'Register Now')]")).click();
+        
+    }
+
+    @Then("MAP type {string} as firstName")
+    public void mapTypeAsFirstName(String firstName) {
+        getDriver().findElement(By.xpath("//input[@formcontrolname='firstName']")).sendKeys(firstName);
+        
+    }
+
+    @Then("MAP type {string} as lastName")
+    public void mapTypeAsLastName(String lastName) {
+        getDriver().findElement(By.xpath("//input[@formcontrolname='lastName']")).sendKeys(lastName);
+
+    }
+
+    @Then("MAP type {string} as email")
+    public void mapTypeAsEmail(String email) {
+        getDriver().findElement(By.xpath("//input[@formcontrolname='email']")).sendKeys(email);
+
+    }
+
+    @And("MAP type {string} as Group Code")
+    public void mapTypeAsGroupCode(String group) {
+        getDriver().findElement(By.xpath("//input[@formcontrolname='group']")).sendKeys(group);
+
+    }
+
+    @Then("MAP type {string} as Password")
+    public void mapTypeAsPassword(String password) {
+        getDriver().findElement(By.xpath("//input[@formcontrolname='password']")).sendKeys(password);
+
+    }
+
+    @Then("MAP click on Register Me button")
+    public void mapClickOnRegisterMeButton() {
+        getDriver().findElement(By.xpath("//span[contains(text(),'Register Me')]")).click();
+
+    }
+
+    @Then("MAP get registered message")
+    public void mapGetRegisteredMessage() {
+        getDriver().findElement(By.xpath("//h4[contains(text(),'You have been Registered.')]")).isDisplayed();
+
+    }
+
+    @And("MAP type {string} as Confirm Password field")
+    public void mapTypeAsConfirmPasswordField(String confirmPassword) {
+        getDriver().findElement(By.xpath("//input[@formcontrolname='confirmPassword']")).sendKeys(confirmPassword);
     }
 }
--- src/test/resources/features/prasad.feature
+++ src/test/resources/features/prasad.feature
@@ -1,14 +1,30 @@
-   @feature1:
-   Feature: To verify that Alphanumeric and special characters are allowed in TEACHER password
-    @Scenario1
-    Scenario:
-      Given MAP open url
+   @feature278:
+   Feature: Registration First Name
+     @Scenario279
+    Scenario: To verify that Alphanumeric and special characters are allowed in First Name
+      Given MAP open url "http://ask-stage.portnov.com"
+       Then MAP click on Register Now button
+       Then MAP type "Madhu" as firstName
+       Then MAP type "T" as lastName
+       Then MAP type "testteer051@gmail.com" as email
+       And MAP type "ABC" as Group Code
+       Then MAP type "12345Abc" as Password
+       And MAP type "12345Abc" as Confirm Password field
+       Then MAP click on Register Me button
+       Then MAP get registered message
 
-    @feature1
-    Feature: My first feature
 
-    @scenario1
-    Scenario: Login operation
-      Given something
+
+
+
+     @feature2:
+     Feature: To verify STUDENT and TEACHER role
+     @Scenario1
+     Scenario:
+       Given MAP open url
+       And MAP type email and password
+       And MAP click on sign in button
+
+
 
 
