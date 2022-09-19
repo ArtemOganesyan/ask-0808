0ebef7d829fd19657caa504ce99e8fe9cd8f8acc
андриано челентано <kryvoruchkoviktoriia@gmail.com>
2022-09-19 12:54:25 -0700

    Password

--- src/test/java/definitions/kryvoruchko.java
+++ src/test/java/definitions/kryvoruchko.java
@@ -1,13 +1,58 @@
 package definitions;
 
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import static java.lang.Thread.sleep;
+import static org.assertj.core.api.Assertions.assertThat;
 import static support.TestContext.getDriver;
 
 public class kryvoruchko {
 
     @Given("I open page {string}")
-    public void iOpenPage(String url) {getDriver().get(amLibrary.url);
+    public void iOpenPage(String url) {
+        getDriver().get(amLibrary.url);
+    }
+
+    @Given("VK open {string} page")
+    public void vkOpenPage(String arg0) {
+        {
+            getDriver().get(amLibrary.url);
+
+        }
+    }
+
+    @Then("VK type {string} into email field")
+    public void vkTypeIntoEmailField(String email) {
+        getDriver().findElement(By.xpath(amLibrary.emailXP)).sendKeys(email);
+    }
+
+    @And("VK type {string} into password field")
+    public void vkTypeIntoPasswordField(String password) {
+        getDriver().findElement(By.xpath(amLibrary.passwordXP)).sendKeys(password);
+    }
+
+    @Then("VK click SignIn button")
+    public void vkClickSignInButton() {
+        getDriver().findElement(By.xpath(amLibrary.signInBtnXP)).click();
+
+    }
+
+    @And("VK wait {int} sec")
+    public void vkWaitSec(int sec) throws InterruptedException {
+        {
+            sleep(sec * 1000);
+        }
     }
 }
+
+
+
+
+
--- src/test/resources/Features/kryvoruchko.feature
+++ src/test/resources/Features/kryvoruchko.feature
@@ -1,14 +1,24 @@
 
   @feature1
     Feature: TestSet - Student: Setting - Full Name
-      Background: Student log in
-#    Given VK open Ask page
-#    Then VK type "thesix1980@santonicrotone.it" into email field
-#    And VK type "12345Abc" into password field
-#    Then VK click SignIn button
-#    And VK wait 5 sec when all elements load
+
 
       @scenario1
       Scenario: Login operation
-        Given I open page "http://ask-stage.portnov.com/"
+        Given I open page "http://ask-stage.portnov.com"
+
+        @scenario2
+        Scenario: Password
+          Given VK open "http://ask-stage.portnov.com" page
+          Then VK type "thesix1980@santonicrotone.it" into email field
+          And VK type "12345Abc" into password field
+          Then VK click SignIn button
+          And VK wait 5 sec
+
+
+
+
+
+
+
 
